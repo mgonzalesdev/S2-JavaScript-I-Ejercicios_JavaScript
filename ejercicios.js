e062bc49dad0cc89405dbd75fdf1b0833251e148
Mariela Gonzales <mariela.gonzales.segobia@gmail.com>
2025-05-06 04:36:20 -0400

    Ejercicio 1.3

--- ejercicios.js
+++ ejercicios.js
@@ -23,7 +23,7 @@ console.log(randomNumber());
 function printNumbers(numbers) {
     numbers.forEach(num => console.log(num));
 }
-let numeros = [1, 2, 3, 4, 5];
+let numeros = [1, 2, 3, 4];
 printNumbers(numeros);
 
 // Ejercicio 5
@@ -67,7 +67,175 @@ console.log(encontrarMaximo(9, 4, 20));
 //  Escribe una función parOImpar que acepte un array de números.
 //  Dentro del bucle, utiliza el operador ternario para determinar si cada número es par o impar.
 function esParImpar(numbers) {
-    numbers.forEach(number =>console.log(number % 2 == 0 ? number + " Es par" : number + " Es impar"));
+    numbers.forEach(number => console.log(number % 2 == 0 ? number + " Es par" : number + " Es impar"));
 }
-esParImpar([1, 2, 3, 4, 6, 8, 7, 9]);
+esParImpar([1, 2, 3, 4, 9]);
 
+// Ejercicio 1.3: Callbacks
+// Nivel 1 Ejercicio 1
+// Callback básico: Escribe una función llamada procesar que acepte dos parámetros: un número y una función de callback.
+// La función procesar debe invocar la función de callback, pasando el número como parámetro.
+function imprimirNumero(numero) {
+    console.log("Imprimiendo un numero desde callback " + numero);
+}
+
+function procesar(num, callback) {
+    callback(num);
+}
+procesar(5, imprimirNumero);
+
+// Ejercicio 2
+// Callbacks con operaciones matemáticas: Escribe una función calculadora que acepte tres parámetros: dos números y una función de callback.
+// La función calculadora debe invocar la función de callback con los dos números como parámetros.
+// Luego, llama calculadora con una función que realice la suma de los dos números.
+function suma(a, b) {
+    return a + b;
+}
+function calculadora(a, b, callback) {
+    let rest = callback(a, b);
+    console.log("La suma es: " + rest);
+}
+calculadora(10, 5, suma);
+
+// Nivel 2 Ejercicio 3
+// Uso de callbacks en funciones asíncronas: Escribe una función esperarISaludar que acepte dos parámetros: un nombre y una función de callback.
+// La función debe esperar 2 segundos y entonces invocar la función de callback, pasando el nombre como parámetro.
+function saludo(nombre) {
+    console.log("Hola " + nombre);
+}
+function esperarISaludar(nombre, fn) {
+    setTimeout(fn(nombre), 2000);
+}
+esperarISaludar("Mariela", saludo);
+
+// Ejercicio 4 Callbacks con arrays: Escribe una función procesarElements que acepte dos parámetros: un array y una función de callback.
+// La función procesarElements debe invocar la función de callback por cada elemento del array.
+function mostrarElemento(elemento) {
+    console.log(elemento);
+}
+
+function procesarElements(elementos, fn) {
+    for (let i = 0; i < elementos.length; i++) {
+        fn(elementos[i]);
+    }
+}
+let frutas = ['naranja', 'limon', 'manzana', 'banana']
+procesarElements(frutas, mostrarElemento);
+
+function procesar1(elementos, fn) {
+    elementos.forEach(element => fn(element));
+}
+let verduras = ["apio", "lechuga"];
+procesar1(verduras, mostrarElemento);
+// Nivel 3 Ejercicio 5
+// Escribe una función procesarCadena que acepte dos parámetros: una cadena de caracteres y una función de callback.
+// La función procesarCadena debe convertir la cadena a mayúsculas y entonces invocar la función de callback con la cadena transformada.
+function mostrarFrase(frase) {
+    console.log(frase);
+}
+
+function procesarCadena(frase, fn) {
+    console.log("Frase inicial: " + frase);
+    frase = frase.toUpperCase();
+    fn(frase);
+}
+procesarCadena("curso de programacion", mostrarFrase);
+
+// Ejercicio 1.4: Rest & Spread operators
+// Nivel 1 Ejercicio 1
+// Operador Spread en Arrays: Crea dos arrays, array1 y array2. Use el operador spread para crear una tercera array que contenga todos 
+// los elementos de array1 y array2.
+const array1 = [2, 4, 6, 8, 10];
+const array2 = [1, 3, 5, 7, 9];
+const array3 = [...array1, ...array2];
+
+console.log(array3);
+// Ejercicio 2
+// Operador Rest en Funciones: Crea una función 'suma' que utilice el operador rest para aceptar un número indeterminado de argumentos y devolver su suma.
+function adicion(...numeros) {
+    let resultado = 0;
+    numeros.forEach(num => { resultado += num });
+    return resultado;
+}
+console.log(adicion(1, 2, 3, 4, 5, 6));
+// Nivel 2 Ejercicio 3
+// Copiando objetos con Spread: Crea un objeto 'objeto1'. Luego crea un segundo objeto, 'objeto2', que sea una copia de 'objeto1' utilizando el operador spread. Cambia una propiedad de 'objeto2' y comprueba que 'objeto1' no ha cambiado.
+
+// Ejercicio 4
+// Resto en Destructuring: Crea un array con varios elementos. Utiliza destructuring y el operador resto para asignar los primeros dos elementos a variables, y después asignar el resto de los elementos a una tercera variable.
+
+// Nivel 3
+// Ejercicio 5
+// Spread en Funciones: Crea una función que acepte tres argumentos. Después, crea un array con tres elementos y llama a la función utilizando el operador spread con esa array.
+
+// Ejercicio 6
+// Fusionando Objetos con Spread: Crea dos objetos con propiedades distintas. Utiliza el operador spread para fusionar estos dos objetos en uno nuevo.
+
+
+
+
+// Ejercicio 1.5: Array transformations
+// Nivel 1 Ejercicio 1
+// Map: Tiene un array de números [1, 2, 3, 4]. Crea un nuevo array que contenga el cuadrado de cada número.
+
+// Ejercicio 2
+// Filtero: Tiene un array de números [1, 2, 3, 4]. Crea un nuevo array que sólo contenga los números pares.
+
+// Ejercicio 3
+// Find: Tiene un array de números [1, 10 , 8, 11]. Use la función find para encontrar el primer número que es mayor a 10.
+
+// Ejercicio 4
+// Reducción: Tiene un array de números [13, 7, 8, 21]. Utiliza la función reduccion para calcular la suma total de los números.
+
+// Nivel 2 Ejercicio 5
+// Dado un array "[ 1, 3, 7, 10 ,15, 17, 11, 5, 8, 12, 9 ]", crea una función en una sola línea que haga lo siguiente:
+// - Filtra los números mayores o iguales a 10.
+// - Multiplica cada número filtrado por 2.
+// - Calcula la suma de los números filtrados y multiplicados por 2.
+// - La función debe devolver el resultado de la suma.
+
+// Nivel 3 Ejercicio 6
+// Every / Some: Usa every y some para determinar si todos o algunos de los elementos del array [11, 12, 13, 14] son ​​mayores que 10, respectivamente
+
+
+
+// Ejercicio 1.6: Array loops
+// Nivel 1 Ejercicio 1
+// forEach: Tiene un array de nombres. Utiliza forEach para imprimir cada nombre en la consola: let nombres = ['Anna', 'Bernat', 'Clara'];
+
+// Ejercicio 2
+// for-of: Tiene un array de nombres. Utiliza un bucle for-of para imprimir cada nombre en la consola: let noms = ['Anna', 'Bernat', 'Clara'];
+
+// Ejercicio 3
+// filtro: Tiene un array de números. Use filtro para crear una nueva array que sólo contenga los números pares. let numeros = [1, 2, 3, 4, 5, 6];
+
+// Nivel 2 Ejercicio 4
+// for-in: Tiene un objeto con pares clave-valor: let obj = { nombre: Ola, edad: 25, ciudad: 'Barcelona' }; Use un bucle for-in para imprimir en la consola cada clave y su valor correspondiente.
+
+// Ejercicio 5
+// for-of con break: Tiene un array de números. Utiliza un bucle for-of para imprimir en la consola los números hasta encontrar el número 5, entonces detiene el bucle: let numeros = [1, 2, 3, 4, 5, 6];
+
+// Nivel 3 Ejercicio 6
+// for-of con index: Utiliza un bucle for-of para imprimir en la consola cada elemento del array y su posición (index): let noms = ['Anna', 'Bernat', 'Clara']
+
+
+
+
+// Ejercicio 1.7: Promisas & Async/Await
+// Nivel 1 Ejercicio 1
+// Creación de una Promesa: Crea una promesa que se resuelva después de 2 segundos y que devuelva la cadena de texto 'Hola, mundo'.
+
+// Ejercicio 2
+// Utilización de una Promesa: Utiliza la promesa creada en el ejercicio anterior. Crea un .then que imprima el resultado en la consola.
+
+// Ejercicio 3
+// Promesa con reject: Crea una promesa que se resuelva después de 2 segundos si el input es igual a 'Hola', y que la rechace si el input es cualquier otra cosa.
+
+// Ejercicio 4
+// Uso de async/await: Escribe una función asíncrona que utilice la función await para esperar el resultado de la promesa creada en el ejercicio 1, y que después imprima este resultado en la consola.
+
+// Nivel 2 Ejercicio 5
+// Gestión de errores con async/await: Modifica la función del ejercicio 4 para que capture cualquier posible error utilizando un blog try/catch.
+
+// Nivel 3 Ejercicio 6
+// Promise.all: Crea dos promesas que se resuelvan después de 2 y 3 segundos, respectivamente. Use Promise.all para esperar que ambas promesas se resuelvan, e imprime los resultados en la consola.
